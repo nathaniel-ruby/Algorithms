8fcc4a7f9e80fc92a910460ab01874b4e3ca5e0c
Nathaniel Ruby <nathaniel.ruby@MacBook-Pro-6.local>
2020-08-04 15:21:46 -0400

    Automated anagram

--- Analysis/Anagram.py
+++ Analysis/Anagram.py
@@ -8,9 +8,22 @@ Author: Nathaniel Ruby
 Date: 08/04/2020                        
 """
 
-def anagramfinder(string1, string2):
+import random
+import string
+
+def string_create(length):
+    letters      = string.ascii_lowercase
+    final_string = ''.join(random.choice(letters) for i in range(length))
+    return final_string
+
+def anagramfinder(chars):
     pos1 = [0]*26
     pos2 = [0]*26
+
+    string1 = string_create(chars)
+    print(string1)
+    string2 = string_create(chars)
+    print(string2)
     
     for i in range(len(string1)):
         alph = ord(string1[i]) - ord("a")
@@ -22,13 +35,13 @@ def anagramfinder(string1, string2):
 
     anagram = True
     j = 0 
-    while j<26 & anagram == True:
+    while j<26 and anagram:
         if pos1[j] == pos2[j]:
             j = j+1
         else:
             anagram = False
     return anagram
 
-print(anagramfinder("peel", "leep"))
+print(anagramfinder(10))
             
         
